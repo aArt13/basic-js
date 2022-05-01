12bf7cfc2c4743743ff05694b3ce2db25c68f69d
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-05-01 15:24:21 +0300

    feat: extended-rep + 1 error

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  function repeater(str, options) {
+//   let res
+//   let newStr = str.toString()
+//   let newAddition = options.addition.toString()
+//   let sep = '+'
+//   let addSep = '|'
+
+//   let addStr = ''
+
+//   if (options.separator) sep = options.separator
+//   if (options.additionSeparator) addSep = options.additionSeparator
+
+//   if (options.addition && options.additionRepeatTimes)   addStr = (newAddition + addSep).repeat(options.additionRepeatTimes - 1) + newAddition
+
+//   if (options.repeatTimes) {
+//       res = (newStr + addStr + sep).repeat(options.repeatTimes - 1) + newStr + addStr
+//   } else {
+//     res = addStr
+//   }
+//   // console.log(res)
+// return res
+// }
+
+
+function repeater(str, options) {
+  let res
+  let sep = '+'
+  let addSep = '|'
+  let addStr = ''
+
+  if (options.separator) sep = options.separator
+  if (options.additionSeparator) addSep = options.additionSeparator
+
+  if (options.addition && options.additionRepeatTimes) {
+    addStr = (options.addition + addSep).repeat(options.additionRepeatTimes - 1) + options.addition
+  } else if (options.addition && options.additionRepeatTimes == null){
+    addStr = (options.addition)
+  } else if (options.addition == false) {
+    addStr = (options.addition.toString() + addSep).repeat(options.additionRepeatTimes - 1) + options.addition.toString()
+  }
+      console.log(addStr)
+
+  if (options.repeatTimes) {
+      res = (str + addStr + sep).repeat(options.repeatTimes - 1) + str + addStr
+  } else {
+    res = str + addStr
+  }
+  console.log(res)
+return res
 }
 
 module.exports = {
